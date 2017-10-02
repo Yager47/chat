9311224654d95ccef9614d0ccd9c253f6fd2987d
TimonYager <ghostship0704@gmail.com>
2017-10-02 15:31:28 +0300

    chat in process

---
+++ server/server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+const server = require('http').Server(app);
+const io = require('socket.io')(server, { serveClient: true });
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/chat', { useMongoClient: true });
+mongoose.Promise = require('bluebird');
+
+nunjucks.configure('./client/views', {
+    autoescape: true,
+    express: app
+});
+
+app.use('/assets', express.static('./client/public'));
+
+app.get('/', (req, res) => {
+  res.render('index.html', { date: new Date() });
+});
+
+require('./sockets')(io);
+
+server.listen(7777, '0.0.0.0', () => {
+  console.log('Server started on port 7777');
+});
---
+++ server/sockets.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const MessageModel = require('./models/messages.model');
+
+module.exports = io => {
+  // .on - what you receive
+  // .emit - what you send
+
+  io.on('connection', function (socket) {
+    socket.emit('connected', "You are connected.");
+
+    // add to room
+    socket.join('all');
+
+    socket.on('msg', content => {
+      const obj = {
+        date: new Date(),
+        content: content,
+        username: socket.id
+      };
+
+      MessageModel.create(obj, err => {
+        if (err) return console.error("MessageModel", err);
+        socket.emit('message', obj);
+        socket.to('all').emit('message', obj);
+        console.log('sent', content);
+      });
+    });
+
+    socket.on('receiveHistory', () => {
+      MessageModel
+        .find({})
+        .sort({date: -1})
+        .limit(50)
+        .sort({date: 1})
+        .lean()
+        .exec( (err, messages) => {
+          if (!err) {
+            socket.emit("history", messages);
+            socket.to('all').emit('history', messages);
+          }
+        })
+    });
+  });
+};
